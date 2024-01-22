ceaa7ee2397d79f1eaa55b3f2cc2127f99cb33a8
clirimkurti <clirimkurti@hennessey.com>
2024-01-22 01:33:04 +0100

    created timber and child theme

--- app/Controller/PageController.php
+++ app/Controller/PageController.php
@@ -2,30 +2,14 @@
 
 namespace Hennessey\Controller;
 
-use Twig\Environment as TwigEnvironment;
-use Hennessey\Model\PageModel;
+use Timber\Timber;
 
 class PageController {
     public function display() {
-        ob_start();
-        wp_head();
-        $wp_head = ob_get_clean();
-
-        ob_start();
-        wp_footer();
-        $wp_footer = ob_get_clean();
-
-        $data = [
-            'posts' => get_posts(),
-            'title' => get_the_title(),
-            'wp_head' => $wp_head,
-            'wp_footer' => $wp_footer,
-        ];
-
-
-        $loader = new \Twig\Loader\FilesystemLoader(MOD_TEMPLATE . '/views');
-        $twig = new \Twig\Environment($loader);
-
-        echo $twig->render('page.twig', $data);
+        $context = Timber::context();
+        $context['posts'] = Timber::get_posts();
+        $context['title'] = get_the_title();
+        
+        Timber::render('page.twig', $context);
     }
 }
